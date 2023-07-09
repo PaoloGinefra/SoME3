415fe0ce4e40903840872589ec660723bb1ea591
darcros <darcros7@gmail.com>
2023-07-09 21:55:23 +0200

    fix p5 accessing window

--- app/p5/SketchRenderer.tsx
+++ app/p5/SketchRenderer.tsx
@@ -1,18 +1,9 @@
 'use client'
 
 import { useEffect, useRef } from 'react'
-import p5 from 'p5'
+import type p5 from 'p5'
+
 import { SketchFunction } from './useSketch'
-
-declare global {
-  interface Window {
-    p5: typeof p5
-  }
-}
-
-if (!window.p5) {
-  window.p5 = p5
-}
 
 export interface SketchRendererProps {
   sketch: SketchFunction
@@ -22,8 +13,20 @@ const SketchRenderer = ({ sketch }: SketchRendererProps) => {
   const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const p5sketch = new p5(sketch, ref.current!)
-    return () => p5sketch.remove()
+    let p5sketch: p5 | null = null
+
+    import('p5').then((p5Module) => {
+      const { default: p5 } = p5Module
+
+      p5sketch = new p5(sketch, ref.current!)
+    })
+
+    return () => {
+      // non sia mai che il componente viene smontato prima che lo sketch sia inizializzato
+      if (p5sketch) {
+        p5sketch.remove()
+      }
+    }
   }, [sketch])
 
   return <div ref={ref}></div>
