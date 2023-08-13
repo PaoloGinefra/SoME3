6a2e58dba03b456e3c0e921366a04da7a23f913d
darcros <darcros7@gmail.com>
2023-08-13 15:52:06 +0200

    chore: only dark theme

--- app/utils/Prose.tsx
+++ app/utils/Prose.tsx
@@ -1,3 +1,3 @@
 export default function Prose(props: any) {
-  return <div className="mx-auto prose dark:prose-invert">{props.children}</div>
+  return <div className="mx-auto prose prose-invert">{props.children}</div>
 }
