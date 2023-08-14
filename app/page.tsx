0b3e3f77d4c222f1c22fda825f737e0017dbbe7f
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-08-14 10:37:23 +0200

    feat: added microsoft clarity

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+"use client"
+import { useEffect } from 'react'
 import { AchievementsProvider } from './Achievements/achievements'
 import Article from './article.mdx'
+import { clarity } from 'react-microsoft-clarity'
+
+
+
 
 export default function Page() {
+  useEffect(() => {
+    clarity.init("ieoz788qcd")
+  })
+
   return <Article />
 }
