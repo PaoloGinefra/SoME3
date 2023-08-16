7421080af601b701ccc1529a331c81e9723c0ffd
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-08-16 17:23:25 +0200

    chore: graphical update

--- app/utils/Video.tsx
+++ app/utils/Video.tsx
@@ -1,17 +1,22 @@
 interface Video_interface {
-    url: string
+    url: string,
+    width: number,
+    height: number
 }
 
-export default function Video({ url }: Video_interface) {
+export default function Video({ url, width, height }: Video_interface) {
     return (
-        <iframe
-            className="m-auto my-10 rounded-lg"
-            width="560"
-            height="315"
-            src={url + "&controls=0&autoplay=1&loop=1&modestbranding=1&mute=1"}
-            title="YouTube video player"
-            allowFullScreen
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; modestbranding"
-        />
+        <div className=" max-w-[90%] overflow-hidden rounded-xl m-auto flex justify-center align-middle" style={{ aspectRatio: width / height }}>
+
+            <iframe
+                className="m-auto my-10 rounded-lg"
+                width={width}
+                height="315"
+                src={url + "&controls=0&autoplay=1&loop=1&modestbranding=1&mute=1"}
+                title="YouTube video player"
+                allowFullScreen
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; modestbranding"
+            />
+        </div>
     )
 }
