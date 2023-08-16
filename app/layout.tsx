04194ab642e765f1bc243043dc6bfcb5e4f78adf
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-08-16 13:05:41 +0200

    chore: added og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'How to programmatically touch grass',
   description: 'A SoME3 entry on drawing plants programmatically.',
+  image: 'https://howtoprogrammaticallytouchgrass.vercel.app/og.png',
 }
 
 export default function RootLayout({
