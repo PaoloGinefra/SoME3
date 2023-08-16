f29c2207a0c8c53a559bae3ea6c2dc8dbbee941b
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-08-16 17:27:10 +0200

    chore: graphical update

--- app/utils/Gif.tsx
+++ app/utils/Gif.tsx
@@ -6,10 +6,10 @@ interface GIF_interface {
 
 export default function Gif({ url, width, height }: GIF_interface) {
     return (
-        <div className=" max-w-[90%] overflow-hidden rounded-xl m-auto flex justify-center align-middle" style={{ aspectRatio: width / height }}>
+        <div className=" max-w-[90%] overflow-hidden rounded-xl m-auto flex justify-center align-middle">
             <iframe
                 className="relative rounded-xl m-auto"
-                style={{ aspectRatio: width / height, transform: "scale(" + (1) + ")" }}
+                style={{ aspectRatio: width / height }}
                 src={url}
                 width={width}
                 height={height}
