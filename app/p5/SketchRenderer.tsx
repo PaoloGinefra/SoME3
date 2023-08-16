4ac4177ed550d1d30375c4c8fa38ba0683346e29
darcros <darcros7@gmail.com>
2023-08-16 11:00:12 +0200

    feat: autoPause sketches when outside viewport

--- app/p5/SketchRenderer.tsx
+++ app/p5/SketchRenderer.tsx
@@ -1,33 +1,61 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import type p5 from 'p5'
 
 import { SketchFunction } from './useSketch'
 
 export interface SketchRendererProps {
   sketch: SketchFunction
+  autoPauseOutsideViewport?: boolean
 }
 
-const SketchRenderer = ({ sketch }: SketchRendererProps) => {
+const SketchRenderer = ({
+  sketch: sketchFunction,
+  autoPauseOutsideViewport: autoPause = true,
+}: SketchRendererProps) => {
   const ref = useRef<HTMLDivElement>(null)
+  const sketch = useRef<p5 | null>(null)
+  const [intersecting, setIntersecting] = useState(false)
 
   useEffect(() => {
-    let p5sketch: p5 | null = null
+    const observer = new IntersectionObserver(([entry]) => {
+      setIntersecting(entry.isIntersecting)
+    })
 
+    observer.observe(ref.current!)
+
+    return () => observer.disconnect()
+  }, [])
+
+  const handleAutoPause = () => {
+    if (!sketch.current) return
+    if (!autoPause) return
+
+    if (intersecting) {
+      sketch.current.loop()
+    } else {
+      sketch.current.noLoop()
+    }
+  }
+  useEffect(() => handleAutoPause(), [intersecting])
+
+  useEffect(() => {
     import('p5').then((p5Module) => {
       const { default: p5 } = p5Module
+      const newSketch = new p5(sketchFunction, ref.current!)
+      sketch.current = newSketch
 
-      p5sketch = new p5(sketch, ref.current!)
+      handleAutoPause()
     })
 
     return () => {
       // non sia mai che il componente viene smontato prima che lo sketch sia inizializzato
-      if (p5sketch) {
-        p5sketch.remove()
+      if (sketch.current) {
+        sketch.current.remove()
       }
     }
-  }, [sketch])
+  }, [sketchFunction])
 
   return <div ref={ref}></div>
 }
