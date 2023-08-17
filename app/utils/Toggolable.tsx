68b5b5cb40bd516796b85c14b1bef79a1682b85a
darcros <darcros7@gmail.com>
2023-08-17 20:27:04 +0200

    fix: calculate <togglable> maxHeight automatically

--- app/utils/Toggolable.tsx
+++ app/utils/Toggolable.tsx
@@ -1,43 +1,55 @@
-import React, { useState } from 'react';
-import { AiOutlineDown, AiOutlineUp } from 'react-icons/ai';
-import Prose from './Prose';
+import React, { useEffect, useRef, useState } from 'react'
+import { AiOutlineDown, AiOutlineUp } from 'react-icons/ai'
+import Prose from './Prose'
 
 interface Props {
-    title: string;
-    children: React.ReactNode;
-    maxHeight: number;
-    opened: boolean;
-    restProps?: any;
+  title: string
+  children: React.ReactNode
+  opened: boolean
+  restProps?: any
 }
 
-export default function Toggolable({ children, title, maxHeight = 3000, opened = true, ...restProps }: Props) {
-    const [open, setOpen] = useState(opened);
-    return (
-        <div className='mt-4'>
-            <div className="cursor-pointer" onClick={() => setOpen(!open)}>
-                <Prose>
-                    <h2 className='flex flex-row gap-2'>
-                        <div className='my-auto'>
-                            {open ? <AiOutlineUp /> : <AiOutlineDown />}
-                        </div>
-                        {title}
-                    </h2>
-                </Prose>
-            </div>
-            <div style={
-                {
-                    maxHeight: open ? maxHeight.toString() + 'px' : '0px',
-                    overflow: 'hidden',
-                    transitionDuration: '1s',
-                    transitionProperty: 'max-height',
-                    transitionTimingFunction: 'ease-in-out'
-                }
-            }
-                className='rounded-lg'
-            >
-                {children}
-            </div>
+export default function Toggolable({
+  children,
+  title,
+  opened = true,
+  ...restProps
+}: Props) {
+  const [open, setOpen] = useState(opened)
 
-        </div>
-    )
+  const ref = useRef<HTMLDivElement>(null)
+  const [height, setHeight] = useState<number | null>(null)
+  const maxHeigth = !!height ? height + 'px' : 'none'
+
+  useEffect(() => {
+    setHeight(ref.current!.scrollHeight)
+  }, [])
+
+  return (
+    <div className="mt-4">
+      <div className="cursor-pointer" onClick={() => setOpen(!open)}>
+        <Prose>
+          <h2 className="flex flex-row gap-2">
+            <div className="my-auto">
+              {open ? <AiOutlineUp /> : <AiOutlineDown />}
+            </div>
+            {title}
+          </h2>
+        </Prose>
+      </div>
+      <div
+        ref={ref}
+        style={{
+          maxHeight: open ? maxHeigth : '0px',
+          overflow: 'hidden',
+          transitionDuration: '1s',
+          transitionProperty: 'max-height',
+          transitionTimingFunction: 'ease-in-out',
+        }}
+        className="rounded-lg"
+      >
+        {children}
+      </div>
+    </div>
+  )
 }
